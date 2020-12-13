316e5644a30ea853ed42dbe435356fb1d30971b0
rich-alien <Richalien@yandex.ru>
2020-12-13 09:24:33 +0400

    роутер для показа специальностей , а так же загатовка для показа групп по специальности

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -7,14 +7,15 @@ router.get('/', async (_request, response) => {
     const users = await usersRepository.getAll();
     response.render('pages/users/view', { users });
 });
-
-router.get('/add', (_request, response) => {
-    response.render('pages/users/add');
+router.get('/specName', async (_request, response) => {
+    const specials = await usersRepository.getSpecName();
+    response.render('pages/users/viewSpecName', { specials });
+});
+router.get('/showGroup', async (_request, response) => {
+    const users = await usersRepository.getSpecName();
+    response.render('pages/users/showGroup', { users });
 });
 
-router.post('/add', async (request, response) => {
-    await usersRepository.add(request.body);
-    response.redirect('/users');
-});
+
 
 module.exports = router;
